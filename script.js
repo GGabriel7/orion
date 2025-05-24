5872a6a3ae93b7d8d8aa0a465c9be1e19b14681e
Gabriel Ramos <gabrielnasciemtoramos002@gmail.com>
2025-05-24 17:21:05 -0300

    correções
    corrigindo botão do whatsapp e mexendo no estilo do teste de velocidade

--- script.js
+++ script.js
@@ -4,4 +4,31 @@ const thumbnails = document.querySelectorAll('.carousel-thumbnails .thumbnail');
     carousel.addEventListener('slid.bs.carousel', function (event) {
         thumbnails.forEach(t => t.classList.remove('active'));
         thumbnails[event.to].classList.add('active');
+}); // js para o carousel
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menu = document.querySelector('.whatsapp-menu');
+  const toggle = menu.querySelector('.whatsapp-toggle');
+  let closeTimeout;
+
+  toggle.addEventListener('click', function (e) {
+    e.stopPropagation();
+    const isActive = menu.classList.toggle('active');
+
+    if (isActive) {
+      clearTimeout(closeTimeout);
+      closeTimeout = setTimeout(() => {
+        menu.classList.remove('active');
+      }, 8000); // Fecha após 8 segundos
+    } else {
+      clearTimeout(closeTimeout);
+    }
+  });
+
+  // Fecha o menu se clicar fora dele
+  document.addEventListener('click', function () {
+    menu.classList.remove('active');
+    clearTimeout(closeTimeout);
+  });
 });
